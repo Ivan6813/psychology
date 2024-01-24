7f96f43bd025584d5d441d73914f5acca8ac7555
Линкевич Иван Владимирович <ilinkevich@alfaleasing.ru>
2024-01-24 13:09:37 +0300

    feat(*): add eslint prettier husky

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <h1>Init project</h1>;
-  </React.StrictMode>
+  </React.StrictMode>,
 );
